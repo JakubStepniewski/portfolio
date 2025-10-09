3a4d7c4172a37cf27a7707f1b4180821a103beb4
REJOWSKI <kubastp12@gmail.com>
2025-10-09 17:13:45 +0200

    navigation plan

--- next-portfolio/app/page.tsx
+++ next-portfolio/app/page.tsx
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[#09090B] min-h-screen bg-[radial-gradient(circle,_#222_1px,_transparent_1px)] [background-size:20px_20px]">
-      <h1 className="text-center pt-20 text-3xl">Tło w kropki 🎯</h1>
+    <div className="bg-[#09090B] min-h-screen bg-[radial-gradient(circle,_#222_1px,_transparent_1px)] [background-size:20px_20px] p-2">
+      <div className="flex justify-between m-5 text-white bg-[#09090B] backdrop-blur-md ml-40 mr-40">
+        <div>
+          <a className="font-bold text-2xl">Jakub Stępniewski</a>
+        </div>
+        <div className="flex gap-10">
+          <a>About</a>
+          <a>Skills</a>
+          <a>Experience</a>
+          <a>Tech stack</a>
+          <a>Projects</a>
+          <a>Contact</a>
+        </div>
+      </div>
     </div>
   );
 }
