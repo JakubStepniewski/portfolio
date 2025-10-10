e1458979cbc155e0b3251b847e95b425ede4d8db
REJOWSKI <kubastp12@gmail.com>
2025-10-10 22:58:43 +0200

    animation + opacity

--- next-portfolio/app/page.tsx
+++ next-portfolio/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-[#09090B] min-h-screen bg-[radial-gradient(circle,_#222_1px,_transparent_1px)] [background-size:20px_20px] p-2">
-      <div className="flex justify-between m-5 text-white bg-[#09090B] backdrop-blur-md ml-40 mr-40">
+      <div className="flex justify-between m-5 text-white bg-zinc-950 backdrop-blur-md mx-40">
         <div>
           <a className="font-bold text-2xl">Jakub Stępniewski</a>
         </div>
@@ -16,6 +16,7 @@ export default function Home() {
           <a>Contact</a>
         </div>
       </div>
+      <div className="w-auto h-90 bg-zinc-900/80 mx-40 mt-30 fade-in"></div>
     </div>
   );
 }
