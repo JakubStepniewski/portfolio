cb9f5c056e945cadbfe36eb18180fd1759505be7
REJOWSKI <kubastp12@gmail.com>
2025-10-13 12:32:06 +0200

    sticky navbar

--- next-portfolio/app/page.tsx
+++ next-portfolio/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="bg-[#09090B] min-h-screen bg-[radial-gradient(circle,_#222_1px,_transparent_1px)] [background-size:20px_20px] p-2">
-      <div className="flex justify-between m-5 text-white bg-zinc-950 backdrop-blur-md mx-40">
+    <div className="bg-[#09090B] min-h-screen bg-[radial-gradient(circle,_#222_1px,_transparent_1px)] [background-size:20px_20px] p-2 md:px-100 px-20 h-700">
+      {/* NAVBAR */}
+
+      <div className="flex justify-between m-5 text-white bg-zinc-950/30 backdrop-blur-md sticky top-0 z-2">
         <div>
           <a className="font-bold text-2xl">Jakub Stępniewski</a>
         </div>
-        <div className="flex gap-10">
+        <div className="flex gap-10 items-center">
           <a>About</a>
           <a>Skills</a>
           <a>Experience</a>
@@ -16,7 +16,9 @@ export default function Home() {
           <a>Contact</a>
         </div>
       </div>
-      <div className="h-90 bg-zinc-900/80 mx-40 mt-30 fade-in"></div>
+
+      {/* PROFILE */}
+      <div className="h-90 bg-zinc-900/80 fade-in mt-30"></div>
     </div>
   );
 }
